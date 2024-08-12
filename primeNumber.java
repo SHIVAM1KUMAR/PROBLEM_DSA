c87e3e9438060d9a4911f8c54ba8fc65c17489b6
SHIVAM1KUMAR <126109527+SHIVAM1KUMAR@users.noreply.github.com>
2024-08-12 11:58:26 +0530

    ..

---
+++ primeNumber.java
@@ -0,0 +1,21 @@
+public class primeNumber{
+    public static void main(String[] args) {
+        int n=91,Count=1;
+     
+            for(int i=2;i<=Math.sqrt(n);i++){
+                if(n%i==0){
+                    Count++;
+                   
+                    if((n/i)!=i){
+                        Count++;
+                    }
+                }
+
+               
+        }
+        if(Count>2){
+            System.out.println("not prime number");
+        }else
+        System.out.println("Prime number");
+    }
+    }
