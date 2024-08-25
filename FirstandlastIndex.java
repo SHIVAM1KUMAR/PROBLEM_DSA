538d4fb3c95dea3a470c8af4c519aab96b322421
SHIVAM1KUMAR <126109527+SHIVAM1KUMAR@users.noreply.github.com>
2024-08-25 13:56:31 +0530

    updating

---
+++ FirstandlastIndex.java
@@ -0,0 +1,16 @@
+public class FirstandlastIndex {
+    public static void main(String[] args) {
+        int arr[]={1,5,7,8,8,9};
+        int n=arr.length-1;
+          
+        for(int i=0;i<n;i++){
+           
+            if(arr[i]==8){
+                arr[i]++;
+
+                System.out.println(i);
+            }
+        }
+       
+    }
+}
